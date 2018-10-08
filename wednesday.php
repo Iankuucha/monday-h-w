b475b4db407e2a0350977d80ca689867e12d965d
iankuucha <iankuucha@gmail.com>
2018-10-08 11:52:09 +0300

    second commit

---
+++ wednesday.php
@@ -0,0 +1,10 @@
+<?php
+class school
+{
+	public function lessonsTaughtToStudents()
+	{
+		echo "maths,english,biology,kiswahili";
+	}
+}
+$Class_room=new school();
+$Class_room->lessonsTaughtToStudents();
